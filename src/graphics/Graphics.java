9f735e928b68952136d13a91537e4a40b20f7286
alexgaaranes <aaaranes@up.edu.ph>
2024-11-25 22:12:56 +0800

    prepared map code

--- src/graphics/Graphics.java
+++ src/graphics/Graphics.java
@@ -9,12 +9,12 @@ package graphics;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
-abstract public class Graphics {
+public class Graphics {
     protected Image image;
     protected double xPos;
     protected double yPos;
 
-    protected Graphics(Image image, double xPos, double yPos) {
+    public Graphics(Image image, double xPos, double yPos) {
         this.image = image;
         this.xPos = xPos;
         this.yPos = yPos;
--- src/graphics/map/Map.java
+++ src/graphics/map/Map.java
@@ -1,11 +1,18 @@
 package graphics.map;
 
+import graphics.Graphics;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Map {
 
     private final int[][] mapMatrix = new int[][] {
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
             {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
-            {0,1,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0},
+            {0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0},
             {0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0},
             {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
             {0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0},
@@ -22,4 +29,17 @@ public class Map {
             {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     };
+    ArrayList<Graphics> mapTiles = new ArrayList<>();
+
+    public Map() {
+        for(int i=0; i<mapMatrix.length; i++) {
+            for(int j=0; j<mapMatrix[0].length; j++) {
+                double xPos = 0; // TODO: Calculate for position base on index
+                double yPos = 0;
+                Graphics tileGraphic = new Graphics(new Image(""), xPos, yPos);
+            }
+        }
+    }
+
+
 }
